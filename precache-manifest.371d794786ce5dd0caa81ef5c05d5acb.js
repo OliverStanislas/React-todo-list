9a33830c203e94394c65539469358356649ac0c0
common <oliverjesingh@gmail.com>
2020-05-22 23:21:51 +0530

    Updates

---
+++ precache-manifest.371d794786ce5dd0caa81ef5c05d5acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35ac941e4489391ea53337f6cafdd4d",
+    "url": "/React-todo-list/index.html"
+  },
+  {
+    "revision": "62349c6cb60785059392",
+    "url": "/React-todo-list/static/css/main.1964cd7c.chunk.css"
+  },
+  {
+    "revision": "6167d4a951513ad94898",
+    "url": "/React-todo-list/static/js/2.c5557e35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-list/static/js/2.c5557e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62349c6cb60785059392",
+    "url": "/React-todo-list/static/js/main.e98e6c2a.chunk.js"
+  },
+  {
+    "revision": "0d3f7d7da87d6e34f7ae",
+    "url": "/React-todo-list/static/js/runtime-main.04ebdddf.js"
+  }
+]);
